1b88ea4eb29036e7f8ea3f35076bb88b8dd6d57b
Krushna Vardhan Reddy Pakkiragari <113488398+KrushnaVardhanReddy@users.noreply.github.com>
2026-07-11 10:28:28 -0400

    chore(e2e): seed baseline schema.sql

--- schema.sql
+++ schema.sql
@@ -2,5 +2,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL DEFAULT 'unknown@example.com'
+    email VARCHAR(255) NOT NULL DEFAULT 'unknown@example.com',
+    ssn VARCHAR(9)
 );
